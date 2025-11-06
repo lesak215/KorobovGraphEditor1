fe66b70ce0a8f004e485a3dd9bd79810779d2974
lesak215 <lesak215@gmail.com>
2025-11-06 22:21:11 +1000

    добавил класс для карандаша

---
+++ KorobovGraphEditor1/classes/DrawingTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace KorobovGraphEditor1.classes
+{
+    public abstract class DrawingTool
+    {
+        public Brush Stroke { get; set; } = Brushes.Black;
+        public double StrokeThickness { get; set; } = 2;
+
+        public abstract void OnMouseDown(Point position);
+        public abstract void OnMouseMove(Point position);
+        public abstract void OnMouseUp(Point position);
+        public abstract Shape GetShape();
+    }
+}
---
+++ KorobovGraphEditor1/classes/PencilTool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace KorobovGraphEditor1.classes
+{ 
+    public class PencilTool : DrawingTool
+    {
+        private Polyline _currentLine;
+
+        public override void OnMouseDown(Point position)
+        {
+            _currentLine = new Polyline
+            {
+                Stroke = Stroke,
+                StrokeThickness = StrokeThickness
+            };
+            _currentLine.Points.Add(position);
+        }
+
+        public override void OnMouseMove(Point position)
+        {
+            if (_currentLine != null)
+            {
+                _currentLine.Points.Add(position);
+            }
+        }
+
+        public override void OnMouseUp(Point position)
+        {
+        }
+
+        public override Shape GetShape()
+        {
+            return _currentLine;
+        }
+    }
+}
